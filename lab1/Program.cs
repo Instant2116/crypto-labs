89c74e57c9029eaac6fc840905de24abbe992d9a
CreatorTheAsh <chess77.aa@gmail.com>
2022-01-06 13:22:15 +0200

    HexStringToByteArray

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -12,7 +12,19 @@ namespace lab1
 
         public static byte[] HexStringToByteArray(string hexString)
         {
-            return null;
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(System.Globalization.CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, System.Globalization.NumberStyles.HexNumber, System.Globalization.CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
         }
     }
 }
