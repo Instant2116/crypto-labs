12f4f02723aaee19908cc828c942bcf44f446ccb
CreatorTheAsh <chess77.aa@gmail.com>
2022-01-06 13:21:20 +0200

    start

--- lab1/1.cs
+++
@@ -1,6 +0,0 @@
-﻿using System;
-
-namespace lab1
-{
-    
-}
---
+++ lab1/BitXor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace lab1
+{
+    public static class BitXor
+    {
+       
+        public static void BruteForce(byte[] encoded, string logPath)
+        {
+            for (int i = 0; i < 255; i++)
+            {
+                byte[] temp = new byte[encoded.Length - 1];
+                for (int j = 0; j < encoded.Length - 1; j++)
+                {
+                    temp[j] = (byte)(encoded[j] ^ i);
+                }
+                string result = System.Text.Encoding.UTF8.GetString(temp);
+                System.IO.File.AppendAllText(logPath, "i:\t" + i + "\n" + result + "\n");
+                Console.WriteLine("i:\t" + i);
+                Console.WriteLine(result);
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+    
+    
+    
+}
